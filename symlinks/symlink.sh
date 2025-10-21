863d48b8a46a6eebdf3e3d674bfd53af5beae225
NotJesus172 <naftalisilvera@gmail.com>
2025-10-21 05:49:24 -0400

    install and config stow

--- symlinks/symlink.sh
+++ symlinks/symlink.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#   Install stow
+sudo dnf -y in stow
+#   Set stow directory
+stow -d ~/.files
+#   Stow package config files
 stow -t ~ -S bash --adopt
 stow -t ~/.config -S starship --adopt
 stow -t ~/.config/kitty -S kitty --adopt
