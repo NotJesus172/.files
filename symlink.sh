bfb614604842162393b15dbc8331a63e715cdead
NotJesus172 <157871959+NotJesus172@users.noreply.github.com>
2025-12-23 23:42:21 -0500

    Include code and swaync in install
    Added removal of previous shell config files and included stow command for 'swaync' and 'code'.

--- symlink.sh
+++ symlink.sh
@@ -2,20 +2,24 @@
 
 #   Install stow
 sudo dnf -y in stow
+
+#   Remove previous shell config files
 rm -rf ~/.bashrc
 rm -rf ~/.zshrc
+
 #   Stow package config files
 
 #   Shell
-stow -S bash 
-stow -S zsh 
-stow -S starship 
+stow -S bash
+stow -S zsh
+stow -S starship
 #   Userspace
-stow -S hypr 
-stow -S waypaper 
-stow -S waybar 
-
+stow -S hypr
+stow -S waypaper
+stow -S waybar
+stow -S swaync
 #  Applications
-stow -S kitty 
-stow -S btop 
-stow -S tor 
+stow -S kitty
+stow -S btop
+stow -S tor
+stow -S code
